5e08c73d9531c5474baafdc8ee9e8d7faec5d730
tynoerm <tinomutendaishemutemaringa@gmail.com>
2025-02-05 00:56:04 +0200

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import cors from "cors";
 
 const app = express();
 app.use(express.json());
+
 const corsOptions = {
   origin: 'https://accounting-system-qlz4.vercel.app', // Your actual frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
@@ -14,41 +15,34 @@ const corsOptions = {
 app.use(express.urlencoded({ extended: true })); 
 app.use(cors(corsOptions));
 
-
-
-// Connecting mongoDB Database
-mongoose.connect("mongodb+srv://accountingsystem:Tinomutenda123#@cluster0.tkqyx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+// Connecting to MongoDB Database with options
+mongoose.connect("mongodb+srv://accountingsystem:Tinomutenda123#@cluster0.tkqyx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  connectTimeoutMS: 30000,  // 30 seconds for connection timeout
+  socketTimeoutMS: 30000,   // 30 seconds for socket timeout
+})
   .then((x) => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
   })
   .catch((err) => {
-    console.error("mongodb+srv://accountingsystem:Tinomutenda123#@cluster0.tkqyx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", err.reason);
+    console.error("MongoDB connection error: ", err);
   });
 
-
-//Importing Routes
-
+// Importing Routes
 import { quotationRoutes } from './routes/SalesModule/quotation.js';
 import { expensesRoutes } from './routes/ExpensesModule/expenses.js';
 import { stocksRoutes } from './routes/StockModule/stocks.js';
 import { salesRoutes } from './routes/SalesModule/sales.js';
 
-
-
-//Route Use
-
+// Route Use
 app.use("/quotation", quotationRoutes);
 app.use("/expense", expensesRoutes);
 app.use("/stock", stocksRoutes);
-app.use("/salesmodel", salesRoutes)
+app.use("/salesmodel", salesRoutes);
 
-
-
-
-
-//Start Server
+// Start Server
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
-    console.log("Connection Established Successfully on " + port);
+  console.log("Connection Established Successfully on " + port);
 });
-
